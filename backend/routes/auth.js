955bd064a8590b4e754dbcf6dc8095201e594baf
AzKir <a.kirykhina@yandex.ru>
2023-08-02 19:06:37 +0300

    Update auth.js
    crash-test code

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,6 +3,12 @@ const { celebrate, Joi } = require('celebrate');
 const { postUser, login } = require('../controllers/users');
 const { urlPattern } = require('../const');
 
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 router.post(
   '/signin',
   celebrate({
